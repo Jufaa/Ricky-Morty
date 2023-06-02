89607ceddb02d3efe9a9c6a8a54652964cd6b6bf
Juan Cruz Francitorra <juanfrancitorra@gmail.com>
2023-06-02 16:34:25 -0300

    errors

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import { ShowPJS } from './ShowPJS';
 import { NavBar } from '../../components/NavBar';
-import { Pagination } from '../../components/Pagination';
 
 const Home = () => {
   return (
